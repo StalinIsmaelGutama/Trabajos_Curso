cc25d1fa3d88f60c569c66f541e89b23a8821775
StalinIsmaelGutamaChuñir <stalingutama2@gmail.com>
2023-03-28 13:33:21 -0500

    Operaciones

---
+++ Operaciones_basicas.py
@@ -0,0 +1,14 @@
+d1 = int(input("Ingrese el primer dato: "))
+d2 = int(input("Ingrese el segundo dato: "))
+
+suma = d1 + d2
+print("La suma es :", suma)
+
+resta = d1 - d2
+print("La resta es :", resta)
+
+multi = d1 * d2
+print("la multiplicacion es:", multi)
+
+divi = d1 / d2
+print("La division es :", divi)
