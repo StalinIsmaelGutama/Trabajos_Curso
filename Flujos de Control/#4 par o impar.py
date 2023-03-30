b96d9730e89fe1d3ce2ac68c1e4cf8eb8c57c793
StalinIsmaelGutamaChuñir <stalingutama2@gmail.com>
2023-03-30 09:46:18 -0500

    Par o impar

---
+++ Flujos de Control/#4 par o impar.py
@@ -0,0 +1,11 @@
+"""
+4. Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+"""
+
+n = int(input("Ingrese el numero"))
+
+if n%2 == 0:
+    print("Es par")
+else:
+    print("Es impar")
+    
