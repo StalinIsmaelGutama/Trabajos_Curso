922962ab6079ebba6ffb11dca4bcb6bcfd6bc450
StalinIsmaelGutama <129073706+StalinIsmaelGutama@users.noreply.github.com>
2023-03-31 08:40:18 -0500

    Solicite un numero
    """
    8.1. Solicita un número y verifica si está dentro del rango del 0 al 9
    """

--- Flujos de Control/#8.1 Solicite un numero.py
+++ Flujos de Control/#8.1 Solicite un numero.py
@@ -1,8 +1,9 @@
 num = input("Ingrese un numero: ")
+num = input("Ingrese un numero: ")
 
 if len(num) != 1:
     print("No se puede procesar el dato.\nDebe ingresar una solo numeros del rango del 0 al 9")
 elif num in ("0123456789"):
-    print("El numero esta dentro del rango")
+    print(f"El numero ingresado: {num} esta dentro del rango")
 else:
-    print("Esta fuera del rango")
+    print(f"El numero ingresado: {num} esta fuera del rango")
