345bd874f7e313bc2faa8f54c01467d98233f5fc
StalinIsmaelGutama <129073706+StalinIsmaelGutama@users.noreply.github.com>
2023-03-31 07:50:55 -0500

    Ingresar dos números y ingresar comandos

---
+++ Flujos de Control/#6 Ingresar dos numeros y calcular.py
@@ -0,0 +1,30 @@
+"""
+6. Realiza un programa que lea dos números por teclado y permita elegir entre 3 opciones en un menú:
+
+- Mostrar una suma de los dos números
+- Mostrar una resta de los dos números (el primero menos el segundo)
+- Mostrar una multiplicación de los dos números
+- En caso de introducir una opción inválida, el programa informará de que no es correcta.
+"""
+
+n1 = int(input("Ingrese el primer valor: "))
+n2 = int(input("Ingrese el segundo valor: "))
+
+n3 = n1 + n2 
+n4 = n1 - n2 
+n5 = n1 * n2 
+
+print("Presione 1 para sumar: ")
+print("Presione 2 pra restar:")
+print("Presione 3 para multiplicar: ")
+
+comando = int(input("Digite una opcion: "))
+
+if comando == 1:
+    print("La suma es: ", n3)
+elif comando == 2:
+    print("La resta es: ", n4)
+elif comando == 3:
+    print("La multiplicacion es: ", n5)
+else:
+    print("Comando no valido")
