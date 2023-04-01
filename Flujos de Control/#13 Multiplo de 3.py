d47528ff886366d0fb20b54d4860b690b9fc30b6
StalinIsmaelGutama <129073706+StalinIsmaelGutama@users.noreply.github.com>
2023-03-31 19:50:43 -0500

    Determinr si es multiplo de 3
    """
    Ejercicio 13. Crear un programa para determinar si un número es múltiplo de 3:
    """

---
+++ Flujos de Control/#13 Multiplo de 3.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio 13. Crear un programa para determinar si un número es múltiplo de 3:
+"""
+
+num = int(input('Digite el número a verfificar si es múltiplo de 3: '))
+
+if num % 3 == 0:
+    print(f"El número {num} es múltiplo de 3")
+else:
+    print(f"El número: {num} no es multiplo de 3")
