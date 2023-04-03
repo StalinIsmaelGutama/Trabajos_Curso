c8a3ec9a2c8622f4e456be792d2db64049b2663a
StalinIsmaelGutamaChuñir <stalingutama2@gmail.com>
2023-04-03 08:09:04 -0500

    Lista de objetos, usando ciclos y condiciones

---
+++ Colecciones/# 0 Lista, Pares, Condiciones.py
@@ -0,0 +1,12 @@
+pares = [1,2,3,4,5,6,7,8,9,10]
+
+for e in pares:
+    if e % 2 == 0:
+        print("el numero es par",e )
+
+
+nombres = ["Sebastian","Sandro","Bruce","Emily","Ariel","Paul","Jhonnathan"]
+print(nombres)
+
+for e in nombres:
+    print(e.upper())
