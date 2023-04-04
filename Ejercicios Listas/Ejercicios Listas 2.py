45cbf95d6b519b99550e628226f15b532fa05708
StalinIsmaelGutama <stalingutama2@gmail.com>
2023-04-04 15:57:18 -0500

    EJERCICIOS DE LISTAS A RESOLVER

--- Ejercicios Listas/Ejercicios Listas 2.py
+++ Ejercicios Listas/Ejercicios Listas 2.py
@@ -4,9 +4,20 @@
 
 #Crea una lista vacía llamada numeros e introduce los números del 1 al 5. Luego, muestra la lista por pantalla.
 
-numeros
+"""
+numeros = []
+
+for e in range(5):
+    numeros.append(int(input("Introduce los numeros del 1 al 5: ")))    
+
+print(f"Los numeros de la lista son: \n\t{numeros}")
+"""
 
 #Crea una lista con los nombres de tus amigos y muestra por pantalla el primer elemento de la lista.
+
+nombres = ['Sandro', 'Sebastian', 'Ariel', 'Paul', 'Bruce', 'Jhonnathan', 'Emily', 'Adriana']
+
+
 #Crea una lista con los números del 1 al 10 y muestra por pantalla los números pares.
 #Crea una lista con los nombres de tus amigos y muestra por pantalla el último elemento de la lista.
 #Crea una lista con los números del 1 al 10 y muestra por pantalla el último elemento de la lista.
