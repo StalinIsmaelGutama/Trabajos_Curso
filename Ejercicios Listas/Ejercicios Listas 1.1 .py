0ef55abfd1ad4f7db1d5c426358b2c3952580690
StalinIsmaelGutamaChuñir <stalingutama2@gmail.com>
2023-04-05 07:35:11 -0500

    Ejercicios de lista #1

---
+++ Ejercicios Listas/Ejercicios Listas 1.1 .py
@@ -0,0 +1,30 @@
+###CURSO DE FUNDAMENTOS DE PYTHON
+#1. Crear una lista de numeros del 1 al 5
+
+
+ventas = [10,20,30,40,50,60,70,80]
+"""
+print(ventas[3])
+"""
+
+#2. Accede al elemto 3 de la lista:
+
+#3. Modifica un elemento de la lista:
+
+"""
+ventas = [10,20,30,40,50,60,70,80]
+ventas[4] = 100
+print(ventas)
+"""
+
+#4. Agrega el elemento 9 al final de la lista
+
+#5. Eliminar el elemento 2 de la lista:
+
+#6. Recorrer una lista con un bucle for:
+
+#7. Ordenar una lista:
+
+#8. Obtener la longitud de una lista:
+
+#9. Comprobar si un elemento está en la lista:
