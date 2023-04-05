65267e1563ca9d950695cba60057ba6b99db75aa
StalinIsmaelGutamaChuñir <stalingutama2@gmail.com>
2023-04-05 10:06:37 -0500

    Ejercicios con diccionarios, Bucles y funciones

--- Colecciones/Tuplas y Diccionarios .py
+++ Colecciones/Tuplas y Diccionarios .py
@@ -35,12 +35,27 @@ print(f"Las Notas finales son: \n\t{instituto['Notas']}")
 
 
 # print("El promedio de las notas de la carrera de Big Data son:\n\t", sum(instituto["Notas"])/len(instituto["Notas"]))
-
+"""
 suma = 0
 for e in instituto["Notas"]:
     suma += e
 
 print("El promedio de las notas de la carrera de Big Data son:\n\t", suma/len(instituto["Notas"]))
+"""
 
 # print("El promedio de las notas de la carrera de Big Data son:\n\t", round(suma/len(instituto["Notas"]),2)) # Para redondear o poner cierto numeros de decimales
 
+print(max(instituto["Notas"])) # para el numero mayor
+print(min(instituto["Notas"])) # para el numero menor 
+
+"""
+print("La posicion de la nota minima\n\t",instituto["Notas"].index(min(instituto["Notas"])))
+print("El profesor:\n\t",instituto["profesores"][instituto["Notas"].index(min(instituto["Notas"]))])
+print("La materia es:\n\t",instituto['materias'][instituto["Notas"].index(min(instituto["Notas"]))])
+ """
+ 
+print("La menor nota es:\n\t" , min(instituto["Notas"]) , 
+      "\nLa posicion de la nota minima es: \n\t" , instituto["Notas"].index(min(instituto["Notas"])) , 
+      "\nEl profesor de la materia es:\n\t",instituto["profesores"][instituto["Notas"].index(min(instituto["Notas"]))],
+      "\nLa materia es:\n\t",instituto['materias'][instituto["Notas"].index(min(instituto["Notas"]))]
+      )
