19c07f1c09e73356500604c56c70ed5b40d832c4
StalinIsmaelGutama <stalingutama2@gmail.com>
2023-04-11 13:21:39 -0500

    ñ

--- Flujos de Control/#14 Divisible entre 4 y 6.py
+++
@@ -1,9 +0,0 @@
-"""
-14. Programa para determinar si un número es divisible por 4 y 6:
-"""
-num= int (input("igresar un valor: "))
-
-if num % 4 == 0 and num % 6 == 0:
-    print(f"El numero {num} es divisible para 4 y 6") 
-
-        
